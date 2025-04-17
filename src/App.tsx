14807daf5caa9252a83e51645d3bcdd120366f2f
Chitransh Srivastava <73604474+chitransh-sr@users.noreply.github.com>
2025-04-17 20:47:05 +0530

    edit: experience updated ui (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Experience from "./components/Experience";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Introduction />
       <About />
+      <Experience/>
       <Skills />
       <Projects />
       <Contact />
